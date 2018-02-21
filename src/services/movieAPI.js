e9cf5823c9f629c7a8597e8ba9d10340d84185bd
G.Provost <grace.g.provost@gmail.com>
2018-02-20 21:50:31 -0800

    movies now show when searched

--- src/services/movieAPI.js
+++ src/services/movieAPI.js
@@ -5,8 +5,7 @@ export function search(query, page = 1) {
   const url = `${BASE_URL}s=${query}&page=${page}&apiKey=${API_KEY}`;
   
   return fetch(url).then(r => {
-    console.log(r.json());
-    if(r.ok) return r;
+    if(r.ok) return r.json();
     return r.then(json => { throw json; }); //return error
   });
 }

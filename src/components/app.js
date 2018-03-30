b280fdadf75fe65b92e6e38855f9d9a58126f3c5
Jack Lomax <lomax15@me.com>
2018-03-30 15:42:37 -0700

    setting up categories and API

---
+++ src/components/app.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { search } from '../services/starWarsApi.js';
+import Search from './search';
+import StarWarsList from './swList';
+import Category from './category';
+
+
+
+
+export default class App extends Component {
+
+  state = {
+    category: null,
+    topic: null,
+    error: null
+  };
+
+
+  searchStarWarsAPI = () => {
+    this.setState({
+      results: null,
+      loading: true,
+      error: null,
+    });
+  
+
+    const { topic } = this.state; 
+
+    search(topic).then(
+      ({ results }) => 
+      { this.setState({ results });
+      },
+      error => {
+        this.setState({ error, results: null });
+      })
+      .then(() => {
+        this.setState({ loading: false });
+      });
+    
+  }; 
+  
+  handleSearch = topic => {
+    this.setState({ topic }, this.searchSWAPI);
+  };
+
+
+  render(){
+    const { results, loading } = this.state;
+    return (
+      <div>
+        <header>
+          <Search onSearch={this.handleSearch}/>
+        </header>
+        <div>
+          <Category/>
+        </div>
+        <div>{loading && 'Loading...'}</div>
+      
+        <div>
+          {results && <StarWarsList results={results}/>}
+        </div>
+          
+      </div>
+
+    );
+  }
+
+} 
---
+++ src/components/category.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+export default class Category extends Component {
+
+
+  handleCategory(){
+
+  }
+
+  render(){
+    return (
+      <select onChange={this.handleCategory}>
+        <option>People</option>
+        <option>Films</option>
+        <option>Planets</option>
+        <option>Species</option>
+        <option>Starships</option>
+        <option>Vehicles</option>
+      </select>
+    );
+  }
+}
